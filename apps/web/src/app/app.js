1635a933149238960ca6c02396606fcc166bace3
Bugra Yuksel <bgryks18@gmail.com>
2022-04-26 13:28:17 +0300

    temel css içeriği hazırlandı

--- apps/web/src/app/app.js
+++ apps/web/src/app/app.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import './app.module.scss';
-const app = () => {
-  return <div>app</div>;
+import styles from './app.module.scss';
+const App = () => {
+  return (
+    <div id="content">
+      <div id="bar"></div>
+      <div id="header">
+        <div>
+          <h1>MagicHesap</h1>
+          <p>Hesap Uzmanı</p>
+        </div>
+      </div>
+      <div id="account">
+        <div id="accountArena"></div>
+      </div>
+    </div>
+  );
 };
 
-export default app;
+export default App;
