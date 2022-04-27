84777cb29a951f66ec02ebcdbf891d12bc1d0826
Bugra Yuksel <bgryks18@gmail.com>
2022-04-27 17:29:22 +0300

    hesap hareketleri arayüzü hazırlandı

--- apps/web/src/app/components/accountActivity.js
+++ apps/web/src/app/components/accountActivity.js
@@ -1,7 +1,75 @@
 import React from 'react';
 import styles from './styles/accountActivity.module.scss';
 const AccountActivity = () => {
-  return <div id="">AccountActivity</div>;
+  return (
+    <div id={styles.accountActivity}>
+      <div>
+        <div className={styles.accountInfo}>
+          <div className={styles.accountFlag}></div>
+          <div className={styles.accountDescription}>
+            <span className={styles.accountName}>Super Hesap</span>
+            <span className={styles.accountDescription}>
+              122980 Türk Lirası (₺)
+            </span>
+          </div>
+        </div>
+        <div className={styles.activityListHeader}>
+          <span className={styles.title}>Hesap Hareketleri</span>
+          <span className={styles.results}>22 Sonuç</span>
+        </div>
+        <div className={styles.activityList}>
+          <div className={styles.activityListItem}>
+            <div>
+              <span className={styles.day}>12</span>
+              <span className={styles.month}>KASIM</span>
+            </div>
+            <div>
+              <span className={styles.process}>Magiclick Market Alışveriş</span>
+              <span className={styles.place}>Market</span>
+            </div>
+          </div>
+          <div className={styles.activityListItem}>
+            <div>
+              <span className={styles.day}>12</span>
+              <span className={styles.month}>KASIM</span>
+            </div>
+            <div>
+              <span className={styles.process}>Magiclick Market Alışveriş</span>
+              <span className={styles.place}>Market</span>
+            </div>
+          </div>
+          <div className={styles.activityListItem}>
+            <div>
+              <span className={styles.day}>12</span>
+              <span className={styles.month}>KASIM</span>
+            </div>
+            <div>
+              <span className={styles.process}>Magiclick Market Alışveriş</span>
+              <span className={styles.place}>Market</span>
+            </div>
+          </div>
+          <div className={styles.activityListItem}>
+            <div>
+              <span className={styles.day}>12</span>
+              <span className={styles.month}>KASIM</span>
+            </div>
+            <div>
+              <span className={styles.process}>Magiclick Market Alışveriş</span>
+              <span className={styles.place}>Market</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.pagination}>
+          <div className={styles.paginationItems}>
+            <div className={`${styles.paginationItem} ${styles.chosen}`}>1</div>
+            <div className={styles.paginationItem}>2</div>
+            <div className={styles.paginationItem}>3</div>
+            <div className={styles.paginationItem}>4</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AccountActivity;
