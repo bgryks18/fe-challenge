7daad568fd78cf62cf3e1336408c33aa80cc25e2
Bugra Yuksel <bgryks18@gmail.com>
2022-04-28 11:48:22 +0300

    buton stilleri, ana css'e alındı. içindeki ikonların pozisyonu ayarlandı

--- apps/web/src/app/components/navigation.js
+++ apps/web/src/app/components/navigation.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styles from './styles/navigation.module.scss';
-import { IoAddSharp } from 'react-icons/io5';
+import { IoAddSharp, IoArrowBackOutline } from 'react-icons/io5';
 const Navigation = () => {
   return (
     <div id={styles.navigation}>
       <div>
         <Link to="/">
-          <Button className={styles.mainPage}>ANA SAYFA</Button>
+          <Button className={styles.mainPage}>
+            <IoArrowBackOutline />
+            ANA SAYFA
+          </Button>
         </Link>
 
         <Link to="/">
