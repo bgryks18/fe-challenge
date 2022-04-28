cffdc3af55042d3179e1042ce84afdd4419e7ee0
Bugra Yuksel <bgryks18@gmail.com>
2022-04-28 23:39:55 +0300

    yeni hesap ekle formunda validasyon işlemleri yapıldı

--- apps/web/src/app/components/newAccount.js
+++ apps/web/src/app/components/newAccount.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 import { IoCloseSharp, IoCheckmarkSharp } from 'react-icons/io5';
 import { Row, Col, Form, Button } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 const NewAccount = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const schema = yup
+    .object({
+      accountName: yup.string().required('Bu alan zorunludur.'),
+      accountType: yup
+        .string()
+        .required('Bu alan zorunludur.')
+        .notOneOf(['Seçiniz'], 'Bu alan zorunludur'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+  const onSubmit = (data) => console.log(data);
   return (
     <div id="specialModalBox">
       <div className="close">
@@ -12,8 +30,8 @@ const NewAccount = () => {
       <div className="title">
         <h2>Yeni Hesap Ekle</h2>
       </div>
-      <div className="formArea">
-        <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <div className="formArea">
           <Row>
             <Col xs={6}>
               <Form.Label htmlFor="accountName">Hesap Adı</Form.Label>
@@ -21,11 +39,15 @@ const NewAccount = () => {
                 type="text"
                 placeholder="Hesap Adı"
                 id="accountName"
+                {...register('accountName')}
               />
+              {errors.accountName?.message && (
+                <p className="error">{errors.accountName?.message}</p>
+              )}
             </Col>
             <Col xs={6}>
               <Form.Label htmlFor="accountType">Hesap Tipi</Form.Label>
-              <Form.Select>
+              <Form.Select {...register('accountType')}>
                 <option>Seçiniz</option>
                 <option>Option 1</option>
                 <option>Option 2</option>
@@ -33,16 +55,19 @@ const NewAccount = () => {
                 <option>Option 4</option>
                 <option>Option 5</option>
               </Form.Select>
+              {errors.accountType?.message && (
+                <p className="error">{errors.accountType?.message}</p>
+              )}
             </Col>
           </Row>
-        </Form>
-      </div>
-      <div className="ok">
-        <Button>
-          <IoCheckmarkSharp />
-          KAYDET
-        </Button>
-      </div>
+        </div>
+        <div className="ok">
+          <Button type="submit">
+            <IoCheckmarkSharp />
+            KAYDET
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
